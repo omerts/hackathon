1f4884013bfa35f499a2a2dd1d1c8d6937b00ad7
Omer Spalter <omers@nakairobotics.com>
2024-03-10 00:58:24 +0200

    Update

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-log_file_path = os.path.join(current_directory, 'log')
+log_file_path = os.path.join(current_directory, 'log/launch.log')
 
 log_level = 'INFO'
 
@@ -11,11 +11,11 @@ os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
 logging.basicConfig(level=getattr(logging, log_level), format="%(asctime)s [%(levelname)s] %(message)s", handlers=[
   RotatingFileHandler(log_file_path, 
-                              mode='a',
-                              maxBytes=26*1024*1024, 
-                              backupCount=2, 
-                              encoding=None, 
-                              delay=0),
+                      mode='a',
+                      maxBytes=26*1024*1024, 
+                      backupCount=2, 
+                      encoding=None, 
+                      delay=0),
   logging.StreamHandler()
 ])
 
